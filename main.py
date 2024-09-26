6163ced3892b2de65164a8bb4845d2981e669887
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-09-26 11:31:43 -0600

    Create Car behaviour

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ screen.tracer(0)
 # of the screen and listen for the "Up" keypress to move the turtle north.
 # If you get stuck, check the video walkthrough in Step 3.
 player = Player()
-
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.up,"Up")
 game_is_on = True
 while game_is_on:
-    time.sleep(0.01)
+    time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
