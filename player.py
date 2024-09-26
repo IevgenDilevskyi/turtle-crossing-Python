ad0deda35517ebf1964cc68c78e5c21a62990c96
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-09-26 12:38:45 -0600

    ADd the Scoreboard and Game Over sequence

--- player.py
+++ player.py
@@ -9,10 +9,19 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.pu()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
 
     def up(self):
         new_y = self.ycor() + MOVE_DISTANCE
         self.goto(self.xcor(), new_y)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
